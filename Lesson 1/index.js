1c5324b9257796652807db5223e96f66b7afa16b
Aleksey Mikhailenko <maks.quantum@yandex.ru>
2022-02-21 20:14:28 +0300

    first commit

---
+++ Lesson 1/index.js
@@ -0,0 +1,24 @@
+console.log('Hello, Javascript');
+
+//1 название планеты
+const ourPlanet = 'Earth';
+console.log(ourPlanet);
+//2 текущее время юзера
+let userTimeCount = "13:01";//current
+console.log(userTimeCount);
+//3 количество статей
+let articleCount = 5;
+console.log(articleCount);
+//4 оплата наличными или нет
+let isCash = true;
+console.log(isCash);
+//5 ФИО
+let userName = "Alex"; 
+console.log(userName);
+//или 
+let userNameData = "Aleksey";
+console.log(userNameData);
+let userLastName = "Mikhailenko";
+console.log(userLastName);
+let userSecondName = "Mikhailovich";
+console.log(userSecondName);
