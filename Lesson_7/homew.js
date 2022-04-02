fd81f1594991257a2baf2fee7228e87a230116f2
Aleksey Mikhailenko <maks.quantum@yandex.ru>
2022-04-02 09:07:14 +0300

    без 11 задачи

---
+++ Lesson_7/homew.js
@@ -0,0 +1,61 @@
+// 1
+//const myArray = ['a', '2b', '3cc'];
+// function lengthsArray(array)  {
+//         const result = myArray.map((str) => str.length);
+//         return result;
+// };
+// console.log(lengthsArray(myArray));
+
+//2
+// const numArray = [2, 3, 5, 7, 11, 13, 17, 19];
+// function currentSums(numbers) {
+//       const result = numbers.reduce((sum, current) => {return sum + current}, 0);
+//       return result; //sum - аккум, current - текущее число, 0 - начальное значение sum
+// }
+// console.log(currentSums(numArray));
+
+//3
+// const numbersArray= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+// let result = numbersArray.filter((a, b) => {if ((a + b) === 7) {return '(a, b)'}});
+// console.log(result);  //- выдает пустой массив;
+
+// //4
+// const myString = 'lorem ipsum dolor sit amet';
+// const result = myString.split(' ').map((w) => w[0]);
+// console.log(result);
+
+// //6
+// const myNumbersArray = [3, 3, 5, 6, 2, 6, 9];
+// const result = myNumbersArray.sort((a, b) => a - b);
+// console.log(result);
+
+//7
+// const arrayOne = [3, 2, 5];
+// const arrayTwo = [6, 4, 3];
+// const arrayThree = [8, 1, 10];
+
+// const result = [...arrayOne, ...arrayTwo, ...arrayThree].sort((a, b) => a - b).join(' ');
+// console.log(result);
+
+//8//  - метод flat() сглаживает подмассивы в одноуровневый массив
+// const myArray = [[1, 2, 3], [4, 5], [6]];
+// const result = myArray.flat(1).reduce((sum, current) => {return sum + current}, 0);
+// console.log(result);
+
+//9 
+// const numArray = [1, 2, 3, 4, 5, 6, 7];
+// let newArray = []; 
+// numArray.forEach((n) => newArray.unshift(n));
+// console.log(newArray);
+
+//10 - undefiend
+// const numArray = [1, 2, 3, 4, 5, 6, 7];
+// const result = numArray.forEach((n) => {
+//     let counter = 0;
+//     let sum = 0;
+//     sum += n;
+//     if (sum >= 10) {
+//         return counter};
+//     counter++;
+// });
+// console.log(result);
--- Lesson_7/homew.ts
+++ Lesson_7/homew.ts
@@ -7,9 +7,21 @@
 // console.log(lengthsArray(myArray));
 
 //2
-const numArray: Array<number> = [2, 3, 5, 7, 11, 13, 17, 19];
-function currentSums(numbers: Array<number>) {
-      const result = numbers.reduce((sum: number, current: number) => {return sum + current}, 0);
-      return result; //sum - аккум, current - текущее число, 0 - начальное значение sum
-}
-console.log(currentSums(numArray));
+// const numArray: Array<number> = [2, 3, 5, 7, 11, 13, 17, 19];
+// function currentSums(numbers: Array<number>) {
+//       const result = numbers.reduce((sum: number, current: number) => {return sum + current}, 0);
+//       return result; //sum - аккум, current - текущее число, 0 - начальное значение sum
+// }
+// console.log(currentSums(numArray));
+
+//3
+// const numbersArray: Array<number> = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+// const result: Array<number> = numbersArray.filter((a: number, b: number) => {(a + b) == 7});
+// console.log(result);  //- выдает пустой массив;
+
+// //4
+// const myString: string = 'lorem ipsum dolor sit amet';
+// const result: Array<string> = myString.push//.map((w: string) => w[0]);
+// console.log(result);
+
+
