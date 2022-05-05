2f0c529ece9ce41cef52e1285fb0074f7bc2254c
Aleksey Mikhailenko <maks.quantum@yandex.ru>
2022-05-05 19:15:37 +0300

    Ходы и действия персонажей

--- FrontEnd техническое задание/task2.js
+++ FrontEnd техническое задание/task2.js
@@ -73,41 +73,88 @@ const moves = [
 // 5 опить кулдаун как функцию и прикрутить CSS свойства
 
 
+//Здоровье Евстафия
+let heroMaxHealt = document.querySelector('.healthE');
+let healthHero = document.createElement('span')
+healthHero.textContent =  +prompt('Выберерти сложность', '')
+heroMaxHealt.append(healthHero);
+
+let openToAttack = true; //на героя возможно напасть
+//Ходы Евстафия
 function action(index)  {
     let ul = document.querySelector('ul');
     let move = document.createElement('li');
     move.textContent = moves[index]['name'];
     return ul.append(move);
-};
-
-//Здоровье Евстафия
-// let heroMaxHealt = document.querySelector('.healthE');
-// let healthHero = document.createElement('span')
-// healthHero.textContent =  +prompt('Выберерти сложность', '')
-// heroMaxHealt.append(healthHero);
-
-
-//Ходы Евстафия
-
+}
+ 
 const heroMoves = document.querySelectorAll('button');
-heroMoves.forEach((el, i) => el.addEventListener('click', () => action(i)));
+heroMoves.forEach((el, i) => el.addEventListener('click', () => {
+    action(i);
+    heroAttack(i);
+}));
 
-
-
+function heroAttack(index) {
+    let phDmg = moves[im]['physicalDmg'];
+    let mgDmg = moves[im]['magicDmg'];
+    let phArmor = moves[im]['physicArmorPercents'];
+    let mgArmor = moves[im]['magicArmorPercents'];
+    let resultAttack = [phDmg, mgDmg]
+    let resultArmor = [phArmor, mgArmor]
+ 
+    openToAttack = false;
+    return [resultAttack, resultArmor];
+    
+}
 // Здоровье Монстра
 let monsterMaxHealt = document.querySelector('.healthM');
 let healthMonster = document.createElement('span')
 healthMonster.textContent = monster.maxHealth;
 monsterMaxHealt.append(healthMonster);
-
 //Ходы  монстра
 function mosterMoveSet() {
     let monsterMove = document.createElement('li');
     monsterMove.textContent = monster.moves[0]['name'];
     ul.append(monsterMove);
-    }
+    };
+
+function actionMonster(index)  {
+        let ul = document.querySelector('ul');
+        let move = document.createElement('li');
+        move.textContent = monster.moves[index]['name'];
+        return ul.append(move);
+};
+const im = Math.floor(Math.random()*(3));
+
+monster.attack = function monsterDmg(index) {
+   actionMonster(index);
+   let phDmg = monster.moves[im]['physicalDmg'];
+   let mgDmg = monster.moves[im]['magicDmg'];
+   let phArmor = monster.moves[im]['physicArmorPercents'];
+   let mgArmor = monster.moves[im]['magicArmorPercents'];
+   let resultAttack = [phDmg, mgDmg]
+   let resultArmor = [phArmor, mgArmor]
+
+   openToAttack = false;
+   return [resultAttack, resultArmor];
+}
+
+//Общие Ходы 
+
+do {
+    let monsterPass = null;
+    let heroPass = null;
+    if (openToAttack) {
+      monsterPass = monster.attack(im);
+    };
+    heroPass = 
     
 
+} while (healthMonster.innerHTML.toNumber() > 0 && healthMonster.Hero.innerHTML.toNumber() > 0) 
+
+function resultDamage() {
+
+}
 
 
 
