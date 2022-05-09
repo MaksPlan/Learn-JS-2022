87fea60e4c27b4a12b53ace418cb3d0ef3880344
Aleksey Mikhailenko <maks.quantum@yandex.ru>
2022-05-09 23:40:14 +0300

    last changes, cooldown func, hide, refactoring

--- FrontEnd техническое задание/task2.js
+++ FrontEnd техническое задание/task2.js
@@ -66,25 +66,15 @@ const moves = [
     },
 ];
 
+
+
 //Деструктуризация статов Лютого
 let {maxHealth: monsterHealth, name, moves: monsterMoveSet} = monster
 const [claw, fireBreath, tail] = monsterMoveSet
-function destructMoveSet(index) {
-    const {name,
-        physicalDmg: phDmg,
-        magicDmg: mgDmg,
-        physicArmorPercents: phArmor,
-        magicArmorPercents: mgArmor
-        } = moves[index]
 
-       let stats = [phDmg, mgDmg, phArmor, mgArmor]
-        return stats;
-}
-function countDamage(damage, armor) {
-    let result = damage - armor;
-    result > 0 ? result : result = 0;
-    return result;
-}
+let heroCoolDownSet = [0, 0, 0, 0];
+let monsterCoolDownSet = [0, 0, 0];
+
 // Здоровье Монстра
 let monsterDiv = document.querySelector('.healthM');
 let healthMonster = document.createElement('span')
@@ -97,80 +87,132 @@ let healthHero = document.createElement('span')
 healthHero.textContent =  +prompt('Выберерти сложность', '')
 let heroEvstf = healthHero.textContent;
 heroMaxHealt.append(healthHero);
+    
 
-let openToAttack = true; //на героя возможно напасть
+let startButton = document.querySelector('.start');
+    startButton.addEventListener('click', () => monsterStepChoise())
+    startButton.addEventListener('click', () => heroMoves.forEach((el) => el.classList.remove('hide')));
+
 //Ходы Евстафия
+let heroMoves = document.querySelectorAll('.moveHero');
+function activeHeroMovement(index) {
+  (heroCoolDownSet[index] === 0) ? heroMoves[index].classList.remove('hide') : heroMoves[index].classList.add('hide');
+}
+let heroStats = [];
+for (let i = 0; i < heroMoves.length; i++) {
+        heroMoves[i].addEventListener('click', () => {
+
+        activeHeroMovement(i)
+        action(i);
+        heroStats = destructMoveSet(i);
+        console.log(heroStats);
+        heroCoolDownSet[i] = heroStats[4];
+       finalDamage();
+       coolDownCounter(heroStats);
+       
+    })
+}
+//Ходы монстра
+let monsterStats = [];
+function actionMonster(index)  {
+
+        let monsterStats = destructMoveSet(index);
+        console.log(monsterStats);
+        if (monsterCoolDownSet[index] === 0) {
+            monsterCoolDownSet[index] = monsterStats[4];
+            let ul = document.querySelector('ul');
+            let move = document.createElement('li');
+            move.textContent = monster.moves[index]['name'];
+            ul.append(move);
+        } else {
+            monsterStepChoise();
+        }
+        return monsterStats;
+};
+
+let im = 0;
+function monsterStepChoise()  {
+    im = Math.floor(Math.random()*(3));//Выбор удара
+     return actionMonster(im);//retutn [monsterStats]
+  
+   } 
+let attackToggler  = heroMaxHealt.classList
+//общие функции
+function finalDamage() {
+      let phDamageForHero = countDamage(monsterStats[0], heroStats[2]);//damage armor
+      let phDamageForMonster = countDamage(heroStats[0], monsterStats[2]);
+      let mgDamageForHero = countDamage(monsterStats[1], heroStats[3]);
+      let mgDamageForMonster = countDamage(heroStats[1], monsterStats[3]);
+      monsterHealth = monsterHealth - (phDamageForMonster + mgDamageForMonster) 
+     heroEvstf = +heroEvstf - (phDamageForHero + mgDamageForHero)
+     monsterDiv.innerHTML = monsterHealth;
+     heroMaxHealt.innerHTML = heroEvstf;
+     chooseWinner();
+     coolDownCounter(monsterStats);
+  
+}
+
+function chooseWinner() {
+    if (monsterHealth <= 0) {
+    let span = document.createElement('span');
+    span.textContent = 'Вы победили'
+    span.style.backgroundColor = 'green';
+    span.style.fontSize = '40px';
+    span.style.height = span.style.width = '40px'
+    document.body.append(span);
+    // heroMoves.forEach((button) => button.setAttribute('disabled', 'disabled'));
+    
+} else if (heroEvstf <= 0) {
+    let span = document.createElement('span');
+    span.textContent = 'Вы проиграли';
+    span.style.backgroundColor = 'green';
+    span.style.fontSize = '40px';
+    span.style.height = span.style.width = '40px'
+    document.body.append(span);
+    document.body.append(span);
+    heroMoves.forEach((button) => button.setAttribute('disabled', 'disabled'));
+}
+else {
+
+    heroStats = [];
+    monsterStats = [];
+    do {
+       setTimeout(() => {
+        monsterStats = monsterStepChoise()
+       }, 2000) 
+   
+      attackToggler.toggle('attack!')
+      
+     } while (attackToggler.includes('attack!'))
+}};
+
+function destructMoveSet(index) {
+    const {name,
+        physicalDmg: phDmg,
+        magicDmg: mgDmg,
+        physicArmorPercents: phArmor,
+        magicArmorPercents: mgArmor,
+        cooldown
+        } = moves[index]
+
+       let stats = [phDmg, mgDmg, phArmor, mgArmor, cooldown]
+        return stats;
+}
+function countDamage(damage = 0, armor = 0) {
+    let result = damage - armor;
+    result > 0 ? result : result = 0;
+    return result;
+}
+
 function action(index)  {
     let ul = document.querySelector('ul');
     let move = document.createElement('li');
     move.textContent = moves[index]['name'];
     ul.append(move);
    }
-const heroMoves = document.querySelectorAll('button');
-let heroStats = [];
-//     heroMoves.map((el, i) => el.addEventListener('click', () => {
-//     action(i);
-//     openToAttack = false;
-//     heroStats = destructMoveSet(i);
-//    return heroStats;
-// }))
-
-for (let i = 0; i < heroMoves.length; i++) {
-        heroMoves[i].addEventListener('click', () => {
-        handler();
-        action(i);
-        openToAttack = false;
-        heroStats = destructMoveSet(i)})
-}
-//Ходы монстра
-
-function actionMonster(index)  {
-        let ul = document.querySelector('ul');
-        let move = document.createElement('li');
-        move.textContent = monster.moves[index]['name'];
-        ul.append(move);
-        let monsterStats = destructMoveSet(index);
-        return monsterStats;
+function coolDownCounter(arr) {
+       arr.map((el) => el === 0 ? 0 : el--)
 };
-//ИИ игры // heroStep //
-
-function handler()  {
-    let im = Math.floor(Math.random()*(3));//Выбор удара
-    // if (openToAttack) {
-        
-
-     let monsterStep = actionMonster(im);//retutn [monsterStats]
-      let phDamageForHero = countDamage(monsterStep[0], heroStats[2]);//damage armor
-      let phDamageForMonster = countDamage(heroStats[0], monsterStep[2]);
-      let mgDamageForHero = countDamage(monsterStep[1], heroStats[3]);
-      let mgDamageForMonster = countDamage(heroStats[1], monsterStep[3]);
-      monsterHealth = monsterHealth - (phDamageForMonster + mgDamageForMonster) 
-     heroEvstf = +heroEvstf - (phDamageForHero + mgDamageForHero)
-     monsterDiv.innerHTML = monsterHealth;
-     heroMaxHealt.innerHTML = heroEvstf;
-     chooseWinner();
-    }; 
-//    } 
-
-function chooseWinner() {if (!(monsterHealth > 0)) {
-    let span = document.createElement('span');
-    span.textContent = 'Вы победили'
-    span.style.backgroundColor = 'green';
-    span.style.fontSize = '40px';
-    span.style.height = span.style.width = '80px'
-    document.body.append(span);
-} else if (!(heroEvstf > 0)) {
-    let span = document.createElement('span');
-    span.textContent = 'Вы проиграли';
-    span.style.backgroundColor = 'green';
-    span.style.fontSize = '40px';
-    span.style.height = span.style.width = '80px'
-    document.body.append(span);
-    document.body.append(span);
-}}
-//вытащить кнопку старт из коллекции действий героя и повесить на нее отдельный слушатель
-//сделать через промис и колбэк и таймер так, чтобы при нажатие на кнопку старт монстр бил сам и бил первым, потом игрок выберает удар
-//Кнопка Disable при определение победителя, остановка счетчика (здоровье может быть только неотрицательным)
 
 
 
